e980abc2a8450550442128d7d75fc9ba33012445
daisuzz <daisaku.rugby@gmail.com>
2023-01-22 11:00:04 +0900

    add groovy script

---
+++ pipelines/create-opensearch-index/create-opensearch-index.groovy
@@ -0,0 +1,17 @@
+node("master"){
+    checkout scm
+    properties([
+        parameters([
+            string(
+                name: 'VERSION',
+                    description: '''\
+                        作成したいインデックスのバージョンを指定(例. v1)
+                        ここで指定したバージョンが「インデックス名-バージョン名」として付与される
+                    '''.stripIndent()
+            )
+        ])
+    ])
+    stage("Create Index"){
+        sh "curl -X PUT http://opensearch:9200/books-${VERSION}"
+    }
+}
